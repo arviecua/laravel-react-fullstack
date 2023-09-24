8f6bdf954e88fca052d4493f4ed83031783b5fb7
arviecua <141235475+arviecua@users.noreply.github.com>
2023-09-24 20:24:59 +0800

    done protected Routes

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.jsx";
+import { ContextProvider } from "./contexts/ContextProvider.jsx";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <ContextProvider>
+            <RouterProvider router={router} />
+        </ContextProvider>
     </React.StrictMode>
 );
