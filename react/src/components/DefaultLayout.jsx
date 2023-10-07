b96bed337c77911d40c66d8c71c8f176a84d13b0
arviecua <141235475+arviecua@users.noreply.github.com>
2023-10-07 18:29:18 +0800

    configured the logout method

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,8 +1,10 @@
 import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
+import { useEffect } from "react";
+import axiosClient from "../axios-client";
 // GRADUATION DAY CHEAT DAY I NEED THIS RESTT :)))
 export default function DefaultLayout() {
-    const { user, token } = useStateContext();
+    const { user, token, setUser, setToken } = useStateContext();
 
     if (!token) {
         return <Navigate to="/login" />;
@@ -10,7 +12,18 @@ export default function DefaultLayout() {
 
     const onLogout = (ev) => {
         ev.preventDefault();
+
+        axiosClient.post("/logout").then(({}) => {
+            setUser({});
+            setToken(null);
+        });
     };
+
+    useEffect(() => {
+        axiosClient.get("/user").then(({ data }) => {
+            setUser(data);
+        });
+    }, []);
 
     return (
         <div id="defaultLayout">
