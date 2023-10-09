2695483075ab7b9d7b929d41434e355065c2f0cd
arviecua <141235475+arviecua@users.noreply.github.com>
2023-10-09 19:53:20 +0800

    logout comment

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->group(function () {
+ 
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+       //route for logout need to be inside this auth sanctum
     Route::post('/logout', [AuthController::class,'logout']);
 
 });
