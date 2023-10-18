c850d01cc429c49518d783162ef2dbf53b99ba16
arviecua <141235475+arviecua@users.noreply.github.com>
2023-10-18 20:07:30 +0800

    comments

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -3,13 +3,17 @@ import { Link } from "react-router-dom";
 import axiosClient from "../axios-client";
 
 export default function Users() {
+
+    // state for user and loading
     const [users, setUser] = useState([]);
     const [loading, setLoading] = useState(false);
 
+    // use effect hook
     useEffect(() => {
         getUsers();
     }, []);
 
+    // get users function collecting users from /user 
     const getUsers = () => {
         setLoading(true);
         axiosClient
