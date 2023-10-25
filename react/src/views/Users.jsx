1cf09520dc5e6535b3d32f5ad0076b9c7044cabd
arviecua <141235475+arviecua@users.noreply.github.com>
2023-10-25 10:25:33 +0800

    displayed userform in users

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import axiosClient from "../axios-client";
 
 export default function Users() {
-
     // state for user and loading
     const [users, setUser] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -13,14 +12,14 @@ export default function Users() {
         getUsers();
     }, []);
 
-    // get users function collecting users from /user 
+    // get users function collecting users from /user
     const getUsers = () => {
         setLoading(true);
         axiosClient
             .get("/users")
             .then(({ data }) => {
                 setLoading(false);
-                console.log(data);
+                setUser(data.data);
             })
             .catch(() => {
                 setLoading(false);
@@ -28,9 +27,56 @@ export default function Users() {
     };
     return (
         <div>
-            <div>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                }}
+            >
                 <h1>Users</h1>
-                <Link to={}>Add new</Link>
+                <Link to="/users/new" className="btn-add">
+                    Add new
+                </Link>
+            </div>
+
+            <div className="card animated fadeInDown">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Create Date</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((u) => (
+                            <tr>
+                                <td>{u.id}</td>
+                                <td>{u.name}</td>
+                                <td>{u.email}</td>
+                                <td>{u.created_at}</td>
+                                <td>
+                                    <Link
+                                        className="btn-edit"
+                                        to={"/users/" + u.id}
+                                    >
+                                        Edit
+                                    </Link>
+                                    &nbsp;
+                                    <button
+                                        onClick={(ev) => onDelete(u)}
+                                        className="btn-delete"
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
